2f2abeebb87b0f449478b160119714987d84187f
Ayana Bando <ayanaflax@yahoo.com>
2018-04-09 14:03:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,6 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << name
-     binding.pry
+    #  binding.pry
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end

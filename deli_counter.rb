9f2487e7740e678b7e2f99e8628c0049799b1ff6
Ayana Bando <ayanaflax@yahoo.com>
2018-04-09 13:30:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ def take_a_number(katz_deli, name)
   if katz_deli.length >= 1
     line = []
     count = 1
-    katz_deli.push do |name, i|
-      line <<"#{i + 1}" "#{name}"
+    katz_deli.push do |name|
+      line << "#{name}"
     end
     puts "Welcome, #{name}. You are number #{count} in line."
     count += 1

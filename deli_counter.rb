82986c0a0b022d1eff4541facc5f8c6804eaff5c
Ayana Bando <ayanaflax@yahoo.com>
2018-04-09 13:26:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length >= 1
+    line = []
+    katz_deli.each_with_index do |customer, i|
+      line << "#{i + 1}. " "#{customer}"
+    end
+    puts "The line is currently: #{line.join(" ")}"
+  else puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length >= 1
+    line = []
+    katz_deli.each_with_index do |name, i|
+      line <<"#{i + 1}" "#{name}"
+    end
+    puts "Welcome, #{name}. You are number #{line} in line."
+  end
+end

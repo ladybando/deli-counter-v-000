719d07d41df6575e1d1bf01a2ec86ea68db6e4eb
Ayana Bando <ayanaflax@yahoo.com>
2018-04-09 14:15:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,6 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else katz_deli.length >= 1
-    puts "Currently serving #{name}."
+    puts "Currently serving Logan."
   end
 end

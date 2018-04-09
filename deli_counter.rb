4fce465e1117381944d6c0ad46a937b0b66187bf
Ayana Bando <ayanaflax@yahoo.com>
2018-04-09 14:01:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    count = 1
     katz_deli << name
+    #  binding.pry
     puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
-    count += 1
-  #  binding.pry
 end
